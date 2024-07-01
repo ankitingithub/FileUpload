442be8ae0caeead965dcfe98145912a1eb51dfb6
ankitingithub <as0617300@gmail.com>
2024-07-02 01:24:08 +0530

    Added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+function App(){
+
+    const [ file, setFile ] = useState(null);
+    const [ progress, setProgress ] = useState({started:false,pc:0}); 
+    const [ msg, setMsg ] = useState(null);
+    
+    function handleUpload(){
+        if (!file) {
+            console.log("No file selected");
+            return;
+        }
+        const fd = new FormData();
+        fd.append('file',file);
+
+        setMsg("Uploading...");
+        setProgress(prevState=>{
+            return {...prevState,started:true }
+        })
+        axios.post('http://httpbin.org/post',fd,{
+            onUploadProgress:(ProgressEvent)=>{ setProgress(prevState=>{
+                return {...prevState,pc:ProgressEvent.progress*100}
+            }) },
+            headers : {
+                "Custom-Header":"value",
+            }
+        })
+        .then(res=>{
+            setMsg("Upload successful");
+            console.log(res.data);
+        })
+        .catch(err=>{
+            setMsg("Upload failed");
+            console.error(err)
+        });
+    }
+
+    return(
+        <div className="App">
+
+        <h1>Uploading file</h1>
+
+        <input onChange={ (e) => {setFile(e.target.files[0])}} type="file"/>
+
+        <button onClick={ handleUpload }> Upload</button>
+
+        { progress.started && <progress max="100" value={progress.pc}></progress> }
+        { msg && <span>{msg }</span> }
+        </div>
+    );
 }
 
 export default App;
